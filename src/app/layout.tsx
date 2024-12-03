dc9b39335a38f7536a31490824ee6284551811c2
patgpt <doitformurphy@gmail.com>
2024-12-03 03:17:46 -0400

    Refactor layout.tsx to streamline font imports and clean up unused font definitions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,10 @@
 import Footer from '@/components/Footer'
 import Navbar from '@/components/navigation/Navbar'
 import clsx from 'clsx'
-import type { Metadata } from 'next'
-import { Lobster, Noto_Sans, Noto_Serif } from 'next/font/google'
+import type {Metadata} from 'next'
 
 import './globals.css'
-
-const fontSerif = Noto_Serif({
-  variable: '--font-serif',
-  weight: '400',
-  subsets: ['latin'],
-})
-
-const fontSans = Noto_Sans({
-  variable: '--font-serif',
-  weight: '400',
-  subsets: ['latin'],
-})
-
-const fontDisplay = Lobster({
-  variable: '--font-display',
-  weight: '400',
-  subsets: ['latin'],
-})
+import {fontSans, fontSerif, fontDisplay} from '@/app/fonts'
 
 export const metadata: Metadata = {
   title: 'Mike Kelly',
@@ -44,8 +26,7 @@ export default function RootLayout({
           'antialiased',
           'bg-base-100',
           'min-h-screen'
-        )}
-      >
+        )}>
         <Navbar />
         {children}
         <Footer />
